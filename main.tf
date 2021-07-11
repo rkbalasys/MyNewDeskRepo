a723100f5ab1e753c128d7ae9cfd7f34d23f299d
rkbalasys <64142884+rkbalasys@users.noreply.github.com>
2021-07-11 21:18:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,6 @@ terraform {
 
 provider "aws" {
   region = "ap-southeast-1"
-  access_key = "${AWS_ACCESS_KEY}"
-  secret_key = "${AWS_SECRET_KEY}"
+#  access_key = "${AWS_ACCESS_KEY}"
+#  secret_key = "${AWS_SECRET_KEY}"
 }
