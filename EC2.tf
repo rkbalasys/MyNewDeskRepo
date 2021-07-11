76ade65c51a2f50e9034b46521b5e0f2b7aa527a
rkbalasys <64142884+rkbalasys@users.noreply.github.com>
2021-07-12 00:27:58 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -1,9 +1,18 @@
+data "aws_ami" "myami" {
+    most_recent = true
+    owners      = ["amazon"]
+    
+    
+    filter {
+        name    = "name"
+        values  = ["amzn2-ami-hvm*"]
+
 resource "aws_instance" "testec2" {
-    ami             = var.ami-id
+    ami             = data.aws_ami.myami.id
     instance_type   = var.ins_type
     private_ip      = var.ip_addr
-   # security_groups = var.sec_grp
+  # security_groups = var.sec_grp
     subnet_id       = var.sub_id
-    # vpc_id          = var.vpcid
+  # vpc_id          = var.vpcid
     
 }
