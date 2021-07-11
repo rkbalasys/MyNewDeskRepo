4f46113515f14855bd842cfb15245cfb805ddf77
rkbalasys <64142884+rkbalasys@users.noreply.github.com>
2021-07-11 21:50:06 +0530

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "testec2" {
     ami             = var.ami-id
     instance_type   = var.ins_type
     private_ip      = var.ip_addr
-    security_group = var.sec_grp
+   # security_groups = var.sec_grp
     subnet_id       = var.sub_id
     # vpc_id          = var.vpcid
     
